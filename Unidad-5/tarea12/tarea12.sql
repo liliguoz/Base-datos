f5e08e8b23c7dbfe807314efdd12fbd25255242f
liliguoz <liliguozeng15@gmail.com>
2024-02-19 18:46:56 +0000

    update

--- Unidad-5/tarea12/tarea12.sql
+++ Unidad-5/tarea12/tarea12.sql
@@ -1,27 +1,76 @@
--- Creación de tabla cliente
+--Creación de tabla cliente
 
 create table cliente (
-   ...> id integer primary key autoincrement,
-   ...> nombre text,
-   ...> apellido1 text,
-   ...> apellido2 text, 
-   ...> ciudad text,
-   ...> categoria integer );
+   id integer primary key autoincrement,
+   nombre text,
+   apellido1 text,
+   apellido2 text, 
+   ciudad text,
+   categoria integer );
 
--- Creación tabla comercial
+--Creación tabla comercial
 
 create table comercial (
-   ...> id integer primary key autoincremental,
-   ...> nombre text,
-   ...> apellido1 text,
-   ...> apellido2 text,
-   ...> categoria real );
+   id integer primary key autoincrement,
+   nombre text,
+   apellido1 text,
+   apellido2 text,
+   categoria real );
 
--- Creación tabla pedido
+--Creación tabla pedido
 
-create table pedido (
-   ...> id integer primary key autoincrement,
-   ...> total real,
-   ...> fecha text,
-   ...> id_cliente integer reference cliente(id),
-   ...> id_comercial integer reference comercial(id) );
+CREATE TABLE pedido (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   total REAL,
+   fecha TEXT,
+   id_cliente INTEGER REFERENCES cliente(id),
+   id_comercial INTEGER REFERENCES comercial(id)
+);
+
+---------------------------------------------------Consultas sobre una tabla--------------------------------------------------------------
+
+--Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
+
+select * from pedido order by fecha desc;
+
+/**
+┌────┬─────────┬────────────┬────────────┬──────────────┐
+│ id │  total  │   fecha    │ id_cliente │ id_comercial │
+├────┼─────────┼────────────┼────────────┼──────────────┤
+│ 15 │ 370.85  │ 2019-03-11 │ 1          │ 5            │
+│ 16 │ 2389.23 │ 2019-03-11 │ 1          │ 5            │
+│ 13 │ 545.75  │ 2019-01-25 │ 6          │ 1            │
+│ 8  │ 1983.43 │ 2017-10-10 │ 4          │ 6            │
+│ 1  │ 150.5   │ 2017-10-05 │ 5          │ 2            │
+│ 3  │ 65.26   │ 2017-10-05 │ 2          │ 1            │
+│ 5  │ 948.5   │ 2017-09-10 │ 5          │ 2            │
+│ 12 │ 3045.6  │ 2017-04-25 │ 2          │ 1            │
+│ 14 │ 145.82  │ 2017-02-02 │ 6          │ 1            │
+│ 9  │ 2480.4  │ 2016-10-10 │ 8          │ 3            │
+│ 2  │ 270.65  │ 2016-09-10 │ 1          │ 5            │
+│ 4  │ 110.5   │ 2016-08-17 │ 8          │ 3            │
+│ 11 │ 75.29   │ 2016-08-17 │ 3          │ 7            │
+│ 6  │ 2400.6  │ 2016-07-27 │ 7          │ 1            │
+│ 7  │ 5760.0  │ 2015-09-10 │ 2          │ 1            │
+│ 10 │ 250.45  │ 2015-06-27 │ 8          │ 2            │
+└────┴─────────┴────────────┴────────────┴──────────────┘
+**/
+
+Devuelve todos los datos de los dos pedidos de mayor valor.
+
+select total from pedido limit 2; 
+Devuelve un listado con los identificadores de los clientes que han realizado algún pedido. Tenga en cuenta que no debe mostrar identificadores que estén repetidos.
+
+Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad total sea superior a 500€.
+
+Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
+
+Devuelve el valor de la comisión de mayor valor que existe en la tabla comercial.
+
+Devuelve el identificador, nombre y primer apellido de aquellos clientes cuyo segundo apellido no es NULL. El listado deberá estar ordenado alfabéticamente por apellidos y nombre.
+
+Devuelve un listado de los nombres de los clientes que empiezan por A y terminan por n y también los nombres que empiezan por P. El listado deberá estar ordenado alfabéticamente.
+
+Devuelve un listado de los nombres de los clientes que no empiezan por A. El listado deberá estar ordenado alfabéticamente.
+
+Devuelve un listado con los nombres de los comerciales que terminan por el o o. Tenga en cuenta que se deberán eliminar los nombres repetidos.
